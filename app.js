30736dc16493c8f6cd28f40e3ddeba008e9a30c3
shubham <85775998+shubhams11-gt@users.noreply.github.com>
2024-06-08 00:59:47 +0530

    commit1

---
+++ app.js
@@ -0,0 +1,124 @@
+var divs = ["Menu1", "Menu2", "Menu3", "Menu4"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+  if(visibleDivId === divId) {
+    //visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for(i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if(visibleDivId === divId) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
+
+// ----------------------------
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333",
+  "#333"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+animateCircles();
+
+
+// -------------------
+
+
+function cardHoverEffect(){
+  document.querySelectorAll('.images .cnt')
+  .forEach(function(cnt){
+      cnt.addEventListener('mousemove',function(dets){
+          document.querySelector('.cursor').children[dets.target.dataset.index].style.opacity=1;
+          showingImage = dets.target
+          document.querySelector('.cursor').children[dets.target.dataset.index].style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
+          showingImage.style.filter="grayscale(1)";
+          document.querySelector('.work ').style.backgroundColor = '#' + dets.target.dataset.color;
+      })
+      cnt.addEventListener('mouseleave',function(dets){
+          document.querySelector('.cursor').children[showingImage.dataset.index].style.opacity=0;
+          showingImage.style.filter="grayscale(0)";
+          document.querySelector('.work ').style.backgroundColor = '#f0f0f0';
+      })
+  })
+}
+
+cardHoverEffect();
+
+// -------------------
+
+
+function locoInitialize(){
+  const scroll = new LocomotiveScroll({
+      el: document.querySelector('.main'),
+      smooth: true
+  });    
+}
+
+locoInitialize();
+
