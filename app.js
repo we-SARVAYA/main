f3f655ac6aa00734b5276d71356d1ed6a6f8f57e
shubham <85775998+shubhams11-gt@users.noreply.github.com>
2024-06-22 20:47:47 +0530

    commit

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ circles.forEach(function (circle, index) {
 window.addEventListener("mousemove", function(e){
   coords.x = e.clientX;
   coords.y = e.clientY;
-  
 });
 function animateCircles() {
   
@@ -136,8 +135,6 @@ function replaceAgain(){
   before.style.display='block';
   after.style.display='none';
 }
-
-
 var before2=  document.querySelector('#before2');
 var after2= document.querySelector('#after2');
 
@@ -152,3 +149,25 @@ function replaceAgain2(){
 }
 
 
+// ------------Faq----------
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion, index) => {
+  const header = accordion.querySelector(".accordion__header");
+  const content = accordion.querySelector(".accordion__content");
+  const icon = accordion.querySelector("#accordion-icon");
+
+  header.addEventListener("click", () => {
+    const isOpen = content.style.height === `${content.scrollHeight}px`;
+
+    accordions.forEach((a, i) => {
+      const c = a.querySelector(".accordion__content");
+      const ic = a.querySelector("#accordion-icon");
+
+      c.style.height = i === index && !isOpen ? `${c.scrollHeight}px` : "0px";
+      ic.classList.toggle("ri-add-line", i !== index || !isOpen);
+      ic.classList.toggle("ri-subtract-fill", i === index && !isOpen);
+    });
+  });
+});
