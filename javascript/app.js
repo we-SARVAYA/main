9554160e179016b003af1b3ac18f8a159ff00861
shubham <85775998+shubhams11-gt@users.noreply.github.com>
2024-06-29 14:13:44 +0530

    commit

--- javascript/app.js
+++ javascript/app.js
@@ -88,27 +88,6 @@ animateCircles();
 
 // -------------------
 
-
-function cardHoverEffect(){
-  document.querySelectorAll('.images .cnt')
-  .forEach(function(cnt){
-      cnt.addEventListener('mousemove',function(dets){
-          document.querySelector('.cursor').children[dets.target.dataset.index].style.opacity=1;
-          showingImage = dets.target
-          document.querySelector('.cursor').children[dets.target.dataset.index].style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
-          showingImage.style.filter="grayscale(1)";
-          document.querySelector('.work ').style.backgroundColor = '#' + dets.target.dataset.color;
-      })
-      cnt.addEventListener('mouseleave',function(dets){
-          document.querySelector('.cursor').children[showingImage.dataset.index].style.opacity=0;
-          showingImage.style.filter="grayscale(0)";
-          document.querySelector('.work ').style.backgroundColor = '#f0f0f0';
-      })
-  })
-}
-
-cardHoverEffect();
-
 // -------------------
 
 function scrollr(){
@@ -267,28 +246,6 @@ tl
 
 // -------------------
 
-
-function cardHoverEffect(){
-  document.querySelectorAll('.images .cnt')
-  .forEach(function(cnt){
-      cnt.addEventListener('mousemove',function(dets){
-          document.querySelector('.cursor').children[dets.target.dataset.index].style.opacity=1;
-          showingImage = dets.target
-          document.querySelector('.cursor').children[dets.target.dataset.index].style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
-          showingImage.style.filter="grayscale(1)";
-          document.querySelector('.work ').style.backgroundColor = '#' + dets.target.dataset.color;
-      })
-      cnt.addEventListener('mouseleave',function(dets){
-          document.querySelector('.cursor').children[showingImage.dataset.index].style.opacity=0;
-          showingImage.style.filter="grayscale(0)";
-          document.querySelector('.work ').style.backgroundColor = '#f0f0f0';
-      })
-  })
-}
-
-cardHoverEffect();
-
-// -------------------
 
 // function animateSvg(){
   
